36d88291332350a505aa161dfafce437036a31c4
Yuvraj Takey <yuvraj.takey@gmail.com>
2019-10-22 11:40:07 +0530

    Java-Basics

---
+++ JAVA_Level_3/1_9Ass/temp.java
@@ -0,0 +1,25 @@
+/*********************************************************************************************************************
+
+
+	@Author 	- Krunal Redkar
+	@Title		- DSSD-JAVA
+
+**********************************************************************************************************************/
+
+ import java.lang.*;
+ import java.util.Scanner;
+ 
+ 
+ // main class
+ public class 
+ {
+ 	// entry point method
+ 	public static void main(String arg[])
+ 	{
+ 		System.out.println("JAVA CODE");
+ 	}
+ }
+ 
+ 
+ 
+ 
